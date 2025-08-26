3ff4ddb985f830b6be6ca71dc6c2ccefa462edf2
Nils Steinmetz <nils_steinmetz@web.de>
2025-08-26 16:52:52 +0200

    insert animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,15 +1,17 @@
 import json
 
+with open ('animals_template.html', 'r') as template:
+    animals_template = template.read()
+
 
 def load_data(file_path):
   """ Loads a JSON file """
   with open(file_path, "r") as handle:
     return json.load(handle)
 
-
 animals_data = load_data('animals_data.json')
-print(animals_data)
 
+output = ''
 for animal in animals_data:
     name_animal = animal['name']
     diet_animal = animal['characteristics']['diet']
@@ -21,11 +23,16 @@ for animal in animals_data:
         has_type = False
 
     if has_type:
-        print(f"Name: {name_animal}\n"
+        output += (f"Name: {name_animal}\n"
               f"Diet: {diet_animal}\n"
               f"Location: {location_animal}\n"
-              f"Type: {type_animal}\n")
+              f"Type: {type_animal}\n\n")
     else:
-        print(f"Name: {name_animal}\n"
+        output += (f"Name: {name_animal}\n"
               f"Diet: {diet_animal}\n"
-              f"Location: {location_animal}\n")
+              f"Location: {location_animal}\n\n")
+
+animals_output = animals_template.replace("__REPLACE_ANIMALS_INFO__", output)
+
+with open ('animals.html', 'w') as out:
+    out.write(animals_output)
